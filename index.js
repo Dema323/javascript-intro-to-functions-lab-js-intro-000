0113b355b40783acb83171cf07fc8636fd284307
Maxime Sateur <maxime.sateur@gmail.com>
2018-06-21 13:44:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function shout(string) {
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase())
 }
 
 function logShout(string){
-  console.log(string.toUpperCase()
+  console.log(string.toUpperCase())
 }
 
 function logWhisper(string){
@@ -17,10 +17,11 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string=string.toLowerCase){
     return "I can't hear you!"
-}
-}
-function sayHiToGrandma(string){
-  if(string=string.toUpperCase){
+  }else if( string=string.toUpperCase){
     return "YES INDEED!"
+  }else if(string="I love you, Grandma."){
+    return"I love you, too."
   }
 }
+
+
